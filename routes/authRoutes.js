d424a5b14c24377659dfaf92d27583dd67bbf8b6
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-03-03 09:59:09 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
-// O:\JobConnector\backend\routes\authRoutes.js
+// routes/authRoutes.js
 const express = require('express');
-const { requestOTP, verifyOTP } = require('../controllers/authController');
+const { requestOTP, verifyOTP, sendWhatsAppOTP } = require('../controllers/authController');
 
 const router = express.Router();
 
+//send whatsapp OTP
+router.post('/send/whatsapp-otp', sendWhatsAppOTP);
+
+// Request OTP
 router.post('/request-otp', requestOTP);
+
+// Verify OTP or bypass
 router.post('/verify-otp', verifyOTP);
 
 module.exports = router;
