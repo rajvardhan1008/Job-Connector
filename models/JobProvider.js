40e26d9914d15f145a3a3ea91aabe4d97a60fa95
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-03-03 09:57:41 +0530

    Update JobProvider.js

--- models/JobProvider.js
+++ models/JobProvider.js
@@ -1,11 +1,12 @@
-// O:\JobConnector\backend\models\JobProvider.js
+// models/JobProvider.js
 const mongoose = require('mongoose');
 
 const jobProviderSchema = new mongoose.Schema({
   companyName: { type: String, required: true },
   hrName: { type: String, required: true },
-  hrWhatsappNumber: { type: String },
-  email: { type: String },
-}, { timestamps: true });
+  hrWhatsappNumber: { type: String, unique: true },
+  email: { type: String, unique: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model('JobProvider', jobProviderSchema);
