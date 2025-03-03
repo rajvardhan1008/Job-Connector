1edf3840f52dd2da1121a418532ba45580d308bd
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-03-03 10:22:59 +0530

    Update JobPosting.js

--- models/JobPosting.js
+++ models/JobPosting.js
@@ -1,17 +1,16 @@
-// models/JobPosting.js
+// O:\JobConnector\backend\models\JobPosting.js
 const mongoose = require('mongoose');
 
 const jobPostingSchema = new mongoose.Schema({
   jobTitle: { type: String, required: true },
-  skillType: { type: String, enum: ['IT', 'Non-IT'], required: true },
-  skills: { type: [String], required: true },
-  experienceRequired: { type: Number, required: true },
-  location: { type: String, required: true },
-  maxCTC: { type: Number, required: true },
-  noticePeriod: { type: String, required: true },
-  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'JobProvider', required: true },
-  createdAt: { type: Date, default: Date.now },
-  applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'JobSeeker' }],
-});
+  skillType: { type: String },
+  skills: [String],
+  experienceRequired: { type: String },
+  location: { type: String },
+  maxCTC: { type: String },
+  noticePeriod: { type: String },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'JobProvider' },
+  applicants: [{ seekerId: { type: mongoose.Schema.Types.ObjectId, ref: 'JobSeeker' } }],
+}, { timestamps: true });
 
 module.exports = mongoose.model('JobPosting', jobPostingSchema);
